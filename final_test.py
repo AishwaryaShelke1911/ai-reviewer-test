8f95e93e6f9f6f9fac0f2c91a16686990f094a61
Aishwarya Shelke <aishwaryashelke@Aishwaryas-MacBook-Pro.local>
2026-04-08 11:14:58 +0530

    add final test

--- final_test.py
+++ final_test.py
@@ -1,10 +1,2 @@
-import requests
-
-def fetch_data(url):
-    # No timeout - can hang indefinitely
-    response = requests.get(url)
-    return response.json()
-
-def eval_expression(expr):
-    # Dangerous: eval() can execute arbitrary code
-    return eval(expr)
+def dangerous_func():
+    eval("code")
